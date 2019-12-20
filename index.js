be0c26ff3088bafa6ead8f5fa34610256f984f47
Alvar Soler <alvar.soler@meteologica.com>
2019-12-20 15:24:00 +0100

    Mejorado css, quitados dupes

--- index.js
+++ index.js
@@ -1,273 +1,3 @@
-/*
-const list = [{
-    "cod": "120",
-    "aditive": "Cochineal",
-    "description": "A natural red colour derived from the bodies of pregnant scale insects.",
-    "type": "animal"
-  }, {
-    "cod": "441",
-    "aditive": "Gelatine",
-    "description": "A thickening, stabilising emulsifying agent made by boiling animal (usually cattle or pigs) skin, ligaments, bones, sinews or other tissue.",
-    "type": "animal"
-  }, {
-    "cod": "469",
-    "aditive": "Sodium Caseinate",
-    "description": "An emulsifier, stabiliser & binder, derived from cow’s milk.",
-    "type": "animal"
-  }, {
-    "cod": "542",
-    "aditive": "Bone Phosphate",
-    "description": "An anti-caking agent made from the degreased steam-extract from animal bones.",
-    "type": "animal"
-  }, {
-    "cod": "901",
-    "aditive": "Beeswax (white & yellow)",
-    "description": "A glazing and polishing agent made from bee honeycomb.",
-    "type": "animal"
-  }, {
-    "cod": "904",
-    "aditive": "Shellac",
-    "description": "Glazing agent obtained from the resin produced by the lac insect.",
-    "type": "animal"
-  }, {
-    "cod": "913",
-    "aditive": "Lanolin",
-    "description": "A yellow waxy substance extracted from sheep’s wool.",
-    "type": "animal"
-  },
-  {
-    "cod": "153",
-    "aditive": "Carbon Black",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "252",
-    "aditive": "Potassium Nitrate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "270",
-    "aditive": "Lactic Acid",
-    "description": "Lactic acid is bitter-tasting acid that forms when certain bacteria combine with lactose (milk or sugar). It is used to impart a tart flavour, as well as in the preservation of some foods. It occurs naturally in the souring of milk and can be found in foods such as cheese (used in coagulation) and yogurt. It’s also used in the production of acid-fermented foods such as pickles and sauerkraut.",
-    "type": "meaby"
-  }, {
-    "cod": "322",
-    "aditive": "Lecithin",
-    "description": "An emulsifier (which allows water & oils to mix); is mostly derived from soy beans, seeds, peanuts and maize. Although it may be derived from eggs, in this form it is declared as ‘egg yolk’ and not as ‘322’.",
-    "type": "meaby"
-  }, {
-    "cod": "325",
-    "aditive": "Sodium Lactate",
-    "description": "A derivative of lactic acid.",
-    "type": "meaby"
-  }, {
-    "cod": "326",
-    "aditive": "Potassium Lactate",
-    "description": "A derivative of lactic acid.",
-    "type": "meaby"
-  }, {
-    "cod": "327",
-    "aditive": "Calcium Lactate",
-    "description": "A derivative of lactic acid.",
-    "type": "meaby"
-  }, {
-    "cod": "328",
-    "aditive": "Ammonium Lactate",
-    "description": "A derivative of lactic acid.",
-    "type": "meaby"
-  }, {
-    "cod": "329",
-    "aditive": "Magnesium Lactate",
-    "description": "A derivative of lactic acid.",
-    "type": "meaby"
-  }, {
-    "cod": "422",
-    "aditive": "Glycerol/Glycerin",
-    "description": "May be produced from animal fats, synthesised from propylene or from fermentation of sugars.",
-    "type": "meaby"
-  }, {
-    "cod": "430",
-    "aditive": "Polyoxyethylene (8) Stearate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "431",
-    "aditive": "Polyoxyethylene (40) Stearate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "432",
-    "aditive": "Polyoxyethylene (20) Sorbitan Monolaurate or Polysorbate 20",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "433",
-    "aditive": "Polyoxyethylene (20) Sorbitan Mono-Oleate or Polysorbate 80",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "434",
-    "aditive": "Polyoxyethylene (20) Sorbitan Monopalmitate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "435",
-    "aditive": "Polyoxyethylene (20) Sorbitan Monostearate or Polysorbate 60",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "436",
-    "aditive": "Polyoxyethylene (20) Sorbitan Tristearate or Polysorbate 65",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "442",
-    "aditive": "Ammonium Phosphatides",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "471",
-    "aditive": "Mono- and Di-Glycerides of Fatty Acids",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "473",
-    "aditive": "Sucrose Esters of Fatty Acids",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "474",
-    "aditive": "Sucroglycerides",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "475",
-    "aditive": "Polyglycerol Esters of Fatty Acids",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "476",
-    "aditive": "Polyglycerol Esters of Interesterified Ricinoleic Acid",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "477",
-    "aditive": "Propylene Glycol Mono- and Di-Esters",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "478",
-    "aditive": "Lactylated Fatty Acid Esters of Glycerol and Propane-1,2 Diol",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "481",
-    "aditive": "Sodium Oleyl or Stearoyl Lactylate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "482",
-    "aditive": "Calcium Oleyl or Stearoyl Lactylate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "483",
-    "aditive": "Stearyl Tartrate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "491",
-    "aditive": "Sorbitan Monostearate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "492",
-    "aditive": "Sorbitan Tristearate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "493",
-    "aditive": "Sorbitan Monolaurate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "494",
-    "aditive": "Sorbitan Mono-Oleate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "495",
-    "aditive": "Sorbitan Monopalmitate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "570",
-    "aditive": "Stearic Acid",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "572",
-    "aditive": "Magnesium Stearate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "585",
-    "aditive": "Ferrous Lactate",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "627",
-    "aditive": "Disodium 5′-Guanylate",
-    "description": "A flavour enhancer, isolated from sardines or yeast extract.",
-    "type": "meaby"
-  }, {
-    "cod": "630",
-    "aditive": "Inosinic Acid",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "631",
-    "aditive": "Disodium Inosinate",
-    "description": "A flavour enhancer, often prepared from meat extract and dried sardines. It can also be a synthetic product made via a microbial synthesis process that begins with a vegetable source.",
-    "type": "meaby"
-  }, {
-    "cod": "635",
-    "aditive": "Sodium 5′-Ribonucleotides",
-    "description": "Flavour enhancer.",
-    "type": "meaby"
-  }, {
-    "cod": "640",
-    "aditive": "Glycine and its Sodium Salt",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "910",
-    "aditive": "L-Cysteine",
-    "description": "A flour treatment agent that can be extracted from duck and chicken feathers.",
-    "type": "meaby"
-  }, {
-    "cod": "920",
-    "aditive": "L-Cysteine Monohydrochloride",
-    "description": "L-Cysteine is a flour treatment agent that can be extracted from duck and chicken feathers.",
-    "type": "meaby"
-  }, {
-    "cod": "921",
-    "aditive": "L-Cysteine Hydrochloride Monohydrate",
-    "description": "L-Cysteine is a flour treatment agent that can be extracted from duck and chicken feathers.",
-    "type": "meaby"
-  }, {
-    "cod": "966",
-    "aditive": "Lactitol",
-    "description": "  ",
-    "type": "meaby"
-  }, {
-    "cod": "1518",
-    "aditive": "Glycerol Mono-, Di- and Tri-Acetate or Tracetin",
-    "description": "  ",
-    "type": "meaby"
-  }
-];
-*/
 let list = [];
 
 fetch('allAditives.json').then(resp => resp.json()).then(json => list = json);
@@ -285,26 +15,27 @@ const results = document.querySelector('#results');
 
 if (!input) throw new Error('No encuentro el input!');
 
+const _template = (aditive) => {
+  return `<div class="item ${aditive.type}">
+    <div class="title">
+      <span>${emojis[aditive.type]} ${aditive.code}</span>
+    </div>
+    <div>${aditive.name}</div>
+  </div>`
+};
+
 const onInput = () => {
   const text = input.value.replace(/e/ig, '').split(' ');
   if (text.length) {
-    const aditives = text.filter(t => t.match(/^(e)?\d{3,4}$/i)).map(t => {
+    const aditivesMap = text.filter(t => t.match(/^(e)?\d{3,4}$/i)).map(t => {
       const sotredAditive = list.find(aditive => aditive.code === t);
       if (sotredAditive) return sotredAditive;
-    });
-    // const aditives = list.filter(aditive => text.includes(aditive.cod));
-    // "cod": "120",
-    // "aditive": "Cochineal",
-    // "description": "A natural red colour derived from the bodies of pregnant scale insects.",
-    // "type": "animal"
-    results.innerHTML = aditives.map(aditive =>
-        `<div class="item ${aditive.type}">
-          <div class="title">
-            <span>${emojis[aditive.type]} ${aditive.code}</span>
-          </div>
-          <div>${aditive.name || 'unknown'}${aditive.description ? ('(' + aditive.description + ')') : ''}</div>
-      </div>`)
-      .join('\n');
+    }).reduce((map, aditive) => {
+      if (!map.has(aditive.code)) map.set(aditive.code, aditive);
+      return map;
+    }, new Map());
+    const aditives = [...aditivesMap.values()];
+    results.innerHTML = aditives.map(aditive => _template(aditive)).join('\n');
   }
 };
 
